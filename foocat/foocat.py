99d49513d6c75346ab3e86f55b5daa78eda5cae5
Mike Chen <mike_T_Chen@hotmail.com>
2020-02-26 14:09:22 -0800

    update foocat

--- foocat/foocat.py
+++ foocat/foocat.py
@@ -30,3 +30,6 @@ def catbind(a, b):
     concatenated = pd.concat([pd.Series(a.astype("str")),
                               pd.Series(b.astype("str"))])
     return pd.Categorical(concatenated)
+
+
+    
